78c203d8580e20ed7a0a80df7df97e8fae8b99f1
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-03-01 11:29:00 +0300

    cleanup

--- tinyfiber/support/compiler.hpp
+++ tinyfiber/support/compiler.hpp
@@ -2,11 +2,5 @@
 
 #include <cstdlib>
 
-// Suppress compiler warnings about unused variable/argument
-#define TINY_UNUSED(x) \
-  do {                 \
-    (void)(x);         \
-  } while (false)
-
 // Unreachable by control flow
 #define TINY_UNREACHABLE() std::abort()
