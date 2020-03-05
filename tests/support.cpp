d712f46e562c73cd5e12abe72e703d9e8f3be226
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-03-05 20:34:01 +0300

    more tests

--- tests/support.cpp
+++ tests/support.cpp
@@ -8,3 +8,15 @@ TEST(StringBuilder, HelloWorld) {
   std::string greeting = StringBuilder() << "Hello" << ", " << "World" << "!";
   ASSERT_EQ(greeting, "Hello, World!");
 }
+
+TEST(StringBuilder, NotOnlyStrings) {
+  char chr = 'X';
+  int answer = 42;
+  bool locked = true;
+
+  std::string result = StringBuilder() << "char = " << chr
+      << ", answer = " << answer << ", locked = " << locked;
+
+  // TODO: booleans
+  ASSERT_EQ(result, "char = X, answer = 42, locked = 1");
+}
