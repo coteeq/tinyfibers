f81c6e3f12627a59e8a6b425de7d4f737014ec97
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-01-23 23:22:19 +0700

    CompileOptions.cmake

---
+++ cmake/CompileOptions.cmake
@@ -0,0 +1,9 @@
+# Compile options
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# https://clang.llvm.org/docs/DiagnosticsReference.html
+add_compile_options(-Wall -Wextra -Wpedantic -g -fno-omit-frame-pointer)
+add_compile_options(-Werror)
