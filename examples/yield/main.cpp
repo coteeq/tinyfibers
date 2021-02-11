8edbabb2ca56923fafa62b238917f4d13e7e0967
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-02-12 01:06:48 +0300

    more examples

---
+++ examples/yield/main.cpp
@@ -0,0 +1,34 @@
+#include <tinyfibers/api.hpp>
+
+#include <iostream>
+
+using namespace tinyfibers;
+
+int main() {
+  RunScheduler([]() {
+    auto h1 = Spawn([]() {
+      std::cout << "2" << std::endl;
+      self::Yield();
+      std::cout << "4" << std::endl;
+    });
+
+    std::cout << "1" << std::endl;
+    self::Yield();
+    std::cout << "3" << std::endl;
+
+    auto h2 = Spawn([]() {
+      std::cout << "5" << std::endl;
+    });
+
+    // running = init, run queue: f1, f2
+
+    self::Yield();
+
+    std::cout << "6" << std::endl;
+
+    h1.Join();
+    h2.Join();
+  });
+
+  return 0;
+}
