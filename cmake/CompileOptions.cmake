521800ece778d144e41a8808d22f7751e9432526
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-02-22 12:55:30 +0300

    conditional -Werror

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -6,4 +6,8 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 # https://clang.llvm.org/docs/DiagnosticsReference.html
 add_compile_options(-Wall -Wextra -Wpedantic -g -fno-omit-frame-pointer)
-add_compile_options(-Werror)
+
+if(TINY_FIBERS_DEVELOPER)
+    message(STATUS "Turn warnings into errors")
+    add_compile_options(-Werror)
+endif()
