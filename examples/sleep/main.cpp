3d8ac1d0c2970d7d6f979237beaadc70277ba459
Roman Lipovsky <roman.lipovsky@gmail.com>
2023-02-12 10:21:03 +0300

    cosmetics

--- examples/sleep/main.cpp
+++ examples/sleep/main.cpp
@@ -13,28 +13,30 @@ using tf::WaitGroup;
 
 using namespace std::chrono_literals;
 
-std::vector<int> SleepSort(std::vector<int> ints) {
-  std::vector<int> sorted_ints;
+std::vector<int> SleepSort(std::vector<int> input) {
+  std::vector<int> output;
 
   WaitGroup wg;
-  for (int value : ints) {
+
+  for (int value : input) {
     wg.Spawn([&, value]() {
       SleepFor(1s * value);
-      sorted_ints.push_back(value);
+      output.push_back(value);
     });
   }
+
   wg.Wait();
 
-  return sorted_ints;
+  return output;
 }
 
 int main() {
   RunScheduler([]() {
-    auto sorted_ints = SleepSort({1, 3, 7, 2, 5, 4, 9, 8, 6});
+    auto sorted = SleepSort({1, 3, 7, 2, 5, 4, 9, 8, 6});
 
     fmt::print("Sorted: ");
-    for (size_t i = 0; i < sorted_ints.size(); ++i) {
-      fmt::print("{}, ", sorted_ints[i]);
+    for (size_t i = 0; i < sorted.size(); ++i) {
+      fmt::print("{}, ", sorted[i]);
     }
     fmt::print("\n");
   });
